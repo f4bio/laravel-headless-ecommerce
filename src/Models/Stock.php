2b109def7d9cf0d7ff6dc2eb6313e41e8c89cbdc
Fabio Tea <iam@f4b.io>
2022-02-11 20:56:31 +0100

    updated dependencies (laravel=9.0); added gitignore&editorconfig

--- src/Models/Stock.php
+++ src/Models/Stock.php
@@ -2,91 +2,89 @@
 
 namespace Bjerke\Ecommerce\Models;
 
-use Bjerke\Bread\Builder\DefinitionBuilder;
 use Bjerke\Bread\Models\BreadModel;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Facades\Lang;
 
 class Stock extends BreadModel
 {
-    protected $appends = [
-        'available_quantity'
-    ];
+  protected $appends = [
+    'available_quantity'
+  ];
 
-    protected function define(): DefinitionBuilder
-    {
-        $this->addFieldHasOne(
-            'product',
-            Lang::get('ecommerce::models.product.singular'),
-            self::$FIELD_REQUIRED,
-            'name,sku',
-            null,
-            [
-                'extra_data' => [
-                    'prefetch' => true,
-                    'display_field' => 'name',
-                    'extra_display_field' => 'sku'
-                ]
-            ]
-        );
+  protected function define(): void
+  {
+    $this->addFieldHasOne(
+      'product',
+      Lang::get('ecommerce::models.product.singular'),
+      self::$FIELD_REQUIRED,
+      'name,sku',
+      null,
+      [
+        'extra_data' => [
+          'prefetch' => true,
+          'display_field' => 'name',
+          'extra_display_field' => 'sku'
+        ]
+      ]
+    );
 
-        $this->addFieldHasOne(
-            'store',
-            Lang::get('ecommerce::models.store.singular'),
-            self::$FIELD_OPTIONAL,
-            'name'
-        );
+    $this->addFieldHasOne(
+      'store',
+      Lang::get('ecommerce::models.store.singular'),
+      self::$FIELD_OPTIONAL,
+      'name'
+    );
 
-        $this->addFieldInt(
-            'current_quantity',
-            Lang::get('ecommerce::fields.current_quantity'),
-            self::$FIELD_REQUIRED,
-            [
-                'default' => 0,
-                'description' => Lang::get('ecommerce::fields.descriptions.current_quantity')
-            ]
-        );
-        $this->addFieldInt(
-            'incoming_quantity',
-            Lang::get('ecommerce::fields.incoming_quantity'),
-            self::$FIELD_OPTIONAL,
-            [
-                'default' => 0,
-                'description' => Lang::get('ecommerce::fields.descriptions.incoming_quantity')
-            ]
-        );
-        $this->addFieldInt(
-            'outgoing_quantity',
-            Lang::get('ecommerce::fields.outgoing_quantity'),
-            self::$FIELD_OPTIONAL,
-            [
-                'default' => 0,
-                'description' => Lang::get('ecommerce::fields.descriptions.outgoing_quantity')
-            ]
-        );
-        $this->addFieldInt(
-            'low_stock_threshold',
-            Lang::get('ecommerce::fields.low_stock_threshold'),
-            self::$FIELD_OPTIONAL,
-            [
-                'description' => Lang::get('ecommerce::fields.descriptions.low_stock_threshold')
-            ]
-        );
-        return new DefinitionBuilder($this);
-    }
+    $this->addFieldInt(
+      'current_quantity',
+      Lang::get('ecommerce::fields.current_quantity'),
+      self::$FIELD_REQUIRED,
+      [
+        'default' => 0,
+        'description' => Lang::get('ecommerce::fields.descriptions.current_quantity')
+      ]
+    );
+    $this->addFieldInt(
+      'incoming_quantity',
+      Lang::get('ecommerce::fields.incoming_quantity'),
+      self::$FIELD_OPTIONAL,
+      [
+        'default' => 0,
+        'description' => Lang::get('ecommerce::fields.descriptions.incoming_quantity')
+      ]
+    );
+    $this->addFieldInt(
+      'outgoing_quantity',
+      Lang::get('ecommerce::fields.outgoing_quantity'),
+      self::$FIELD_OPTIONAL,
+      [
+        'default' => 0,
+        'description' => Lang::get('ecommerce::fields.descriptions.outgoing_quantity')
+      ]
+    );
+    $this->addFieldInt(
+      'low_stock_threshold',
+      Lang::get('ecommerce::fields.low_stock_threshold'),
+      self::$FIELD_OPTIONAL,
+      [
+        'description' => Lang::get('ecommerce::fields.descriptions.low_stock_threshold')
+      ]
+    );
+  }
 
-    public function product(): BelongsTo
-    {
-        return $this->belongsTo(config('ecommerce.models.product'));
-    }
+  public function product(): BelongsTo
+  {
+    return $this->belongsTo(config('ecommerce.models.product'));
+  }
 
-    public function store(): BelongsTo
-    {
-        return $this->belongsTo(config('ecommerce.models.store'));
-    }
+  public function store(): BelongsTo
+  {
+    return $this->belongsTo(config('ecommerce.models.store'));
+  }
 
-    public function getAvailableQuantityAttribute(): int
-    {
-        return ($this->current_quantity - $this->outgoing_quantity) ?: 0;
-    }
+  public function getAvailableQuantityAttribute(): int
+  {
+    return ($this->current_quantity - $this->outgoing_quantity) ?: 0;
+  }
 }
